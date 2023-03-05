f70e82a7e3a4a883a531d1ad1f66ddcc9ba1774d
amitkrispin <akrispin50@gmail.com>
2023-03-05 09:12:18 +0200

    files

--- AOS_cart_page.py
+++ AOS_cart_page.py
@@ -16,22 +16,25 @@ class Cart:
     def table_rows(self):
         return self.table_cart().find_elements(By.CSS_SELECTOR, "tbody>tr")
 
-    def item_name_table(self) :
+    def item_name_table(self):
         rows = self.table_rows()
-        title=[]
+        title = []
         for row in rows:
             td_list = row.find_elements(By.TAG_NAME, "td")
             name = td_list[1].text
             quantity = td_list[4].text
-            x=td_list[5].text.index("\n")
+            x = td_list[5].text.index("\n")
             price = td_list[5].text[1:x]
-            title.append([name ,quantity ,price])
+            title.append([name, quantity, price])
         return title
+
     def cartshopping(self):
-        return self.driver.find_element(By.CLASS_NAME,"select")
-    def edit_button(self,num:int):
-        button= self.driver.find_elements(By.CSS_SELECTOR,".edit")
-        return button[num-1].click()
-    def remove_button(self,num:int):
-        but=self.driver.find_elements(By.CSS_SELECTOR,".remove")
-        return but[num-1].click()
+        return self.driver.find_element(By.CLASS_NAME, "select")
+
+    def edit_button(self, num: int):
+        button = self.driver.find_elements(By.CSS_SELECTOR, ".edit")
+        return button[num - 1].click()
+
+    def remove_button(self, num: int):
+        but = self.driver.find_elements(By.CSS_SELECTOR, ".remove")
+        return but[num - 1].click()
---
+++ test_AOS_cart_page.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from AOS_home_page import Home_page
+from AOS_cart_page import Cart
+from AOS_icons_bar import AOS_icons_bar
+from AOS_sign_in_page import sign_in
+from AOS_products_pages import AOS_products
+from AOS_categories_pages import AOS_categories
+from AOS_New_Account import New_Account
+from AOS_my_orders_page import My_orders
+
+
+class Test_AOS_Site(TestCase):
+    pass
